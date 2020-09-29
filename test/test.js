880e9733368b9f75158be290d5c63638e1040947
NorthernMan54 <buyminivan-northern@yahoo.ca>
2020-09-29 14:11:07 -0400

    WIP

--- test/test.js
+++ test/test.js
@@ -1,74 +1,9 @@
-const Bluetooth = require('node-web-bluetooth');
+let device = require('../node_modules/atorch-service').AtorchService;
 
-const UUID_SERVICE = '0000ffe0-0000-1000-8000-00805f9b34fb';
-const UUID_CHARACTERISTIC = '0000ffe1-0000-1000-8000-00805f9b34fb';
+console.log(device);
 
-class SelectFirstFoundDevice extends Bluetooth.RequestDeviceDelegate {
+device.connect();
 
-  // Select first device found
-  onAddDevice(device) {
-    console.log(device.toString());
-    console.log(JSON.stringify(device));
-    this.resolve(device);
-  }
-  onUpdateDevice(device) {
-    // Called whenever new advertisement data was received
-    // for the device
-    console.log(device);
-  }
-
-  // Time-out when device hasn't been found in 20 secs
-  onStartScan() {
-    this._timer = setTimeout(() => {
-      this.reject(new Error('No device found'));
-      process.exit();
-    }, 20000);
-  }
-  onStopScan() {
-    if (this._timer) clearTimeout(this._timer);
-  }
-}
-
-async function connect() {
-  const device = await Bluetooth.requestDevice({
-      filters: [{
-        services: [0xffe0]
-      }],
-      delegate: new SelectFirstFoundDevice()
-    })
-    .then(device => device.gatt.connect())
-    .then(server => {
-      // Getting Battery Service...
-      console.log('Server');
-      return server.getPrimaryService(0xffe0);
-    })
-    .then(service => {
-      console.log('Getting Characteristics...');
-      // Get all characteristics.
-      return service.getCharacteristic(0xffe1);
-    })
-    .then(characteristic => {
-			myCharacteristic = characteristic;
-	    return myCharacteristic.startNotifications().then(_ => {
-	      console.log('> Notifications started');
-	      myCharacteristic.addEventListener('characteristicvaluechanged',
-	          handleNotifications);
-	    });
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-connect();
-
-function handleNotifications(event) {
-  let value = event.target.value;
-  let a = [];
-  // Convert raw data bytes to hex values just for the sake of showing something.
-  // In the "real" world, you'd use data.getUint8, data.getUint16 or even
-  // TextDecoder to process raw data bytes.
-  for (let i = 0; i < value.byteLength; i++) {
-    a.push( ('00' + value.getUint8(i).toString(16)).slice(-2));
-  }
-  console.log('> 0x' + a.join(' '));
-}
+device.on('packet', function(packet) {
+  console.log('Packet', packet);
+})
